565151aad4d44596c000636e09a96addc36fc599
cajsaH96 <58832821+cajsaH96@users.noreply.github.com>
2019-12-12 17:19:52 -0800

    Update TV.java

--- src/Model/TV.java
+++ src/Model/TV.java
@@ -1,4 +1,43 @@
 package Model;
 
 public class TV {
+  private String mRating;
+    private int mTime;
+
+    public TV(String rating, int time) {
+        //super()
+        mRating = rating;
+        mTime = time;
+    }
+
+    public String getRating() {
+        return mRating;
+    }
+
+    public void setRating(String rating) {
+        mRating = rating;
+    }
+
+    public int getTime() {
+        return mTime;
+    }
+
+    public void setTime(int time) {
+        mTime = time;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof TV)) return false;
+        TV TV = (TV) o;
+        return mTime == TV.mTime &&
+                mRating.equals(TV.mRating);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(mRating, mTime);
+    }
 }
+
